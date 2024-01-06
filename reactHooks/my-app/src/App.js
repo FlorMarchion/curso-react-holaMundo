f3912b975063cca6a0b20d8bbf12855aab0b934b
Florencia Marchiondelli <fflor777@hotmail.com>
2024-01-06 19:51:47 -0300

    Custom hook

--- reactHooks/my-app/src/App.js
+++ reactHooks/my-app/src/App.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState, Component } from 'react'
 
-const App = () => {
-    const [contador, setContador] = useState(0) //cero es el valor inicale
-    return(
-        <div>
-            Contador:{contador}
-            <button onClick={()=>setContador(contador+1)}>Incrementar</button>
-        </div>
-    )
-}
+
+//-------> useState en componente Funcional <-------------
+
+// const App = () => {
+//     const [contador, setContador] = useState(0) //cero es el valor inicale
+//     return(
+//         <div>
+//             Contador:{contador}
+//             <button onClick={()=>setContador(contador+1)}>Incrementar</button>
+//         </div>
+//     )
+// }
+
+// useState en componente de clase
+    // class App extends Component {
+    //     state = {contador : 0}
+    //     incrementar = () => {
+    //         this.setState({contador:this.state.contador + 1})
+    //     }
+    //     render(){
+    //         return (
+    //             <div>
+    //                 contador: {this.state.contador}
+    //                 <button onClick={this.incrementar}>Incrementar</button>
+    //             </div>
+    //         )
+    //     }
+    // }
+
+
+    //----> CUSTOM HOOK <---------
+    //Uso el mismo ejemplo de componente funcional, pero customizado.
+    const useContador = (inicial) => {
+        const [contador, setContador] = useState(inicial) //le paso el valor inicial del parametro de useCOntador
+        const incrementar = () =>{
+            setContador(contador + 1)
+        }
+        return [contador, incrementar]
+
+    }
+
+    const App = () => {
+        const [contador, incrementar] = useContador(0)//se otorga el valor inicial
+        return(
+            <div>
+                Contador:{contador}
+                <button onClick={incrementar}>Incrementar</button>
+            </div>
+        )
+        }
 
 export default App;
 
