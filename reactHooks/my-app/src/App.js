f18882ad84d44897542dcefcae06c219e9e14aa6
Florencia Marchiondelli <fflor777@hotmail.com>
2024-01-06 20:26:13 -0300

    useEffect

--- reactHooks/my-app/src/App.js
+++ reactHooks/my-app/src/App.js
@@ -1,69 +1,44 @@
-import { useState, Component } from 'react'
+import { useState, useEffect } from "react"
+/*
 
+----UseEffect-----
 
-//-------> useState en componente Funcional <-------------
+Genera efectos laterales en nuestra aplicación, por ejemplo: actualizar el DOM, leer en DB,
+escribir en DB, conectarnos a una api. Es algo que va a efectuar un cambio dentro de nuestro componente.
 
-// const App = () => {
-//     const [contador, setContador] = useState(0) //cero es el valor inicale
-//     return(
-//         <div>
-//             Contador:{contador}
-//             <button onClick={()=>setContador(contador+1)}>Incrementar</button>
-//         </div>
-//     )
-// }
+En el siguiente ejemplo voy a actualizar el título de la app.
 
-// useState en componente de clase
-    // class App extends Component {
-    //     state = {contador : 0}
-    //     incrementar = () => {
-    //         this.setState({contador:this.state.contador + 1})
-    //     }
-    //     render(){
-    //         return (
-    //             <div>
-    //                 contador: {this.state.contador}
-    //                 <button onClick={this.incrementar}>Incrementar</button>
-    //             </div>
-    //         )
-    //     }
-    // }
+Sintaxis del useEffect:
+useEffect(() => {}, [])
 
+Explicación:
+useEffect((funcion) => {lógica que va a ejecutar}, [Dependencias que necesita para poder funcionar])
 
-    //----> CUSTOM HOOK <---------
-    //Uso el mismo ejemplo de componente funcional, pero customizado.
-    const useContador = (inicial) => {
-        const [contador, setContador] = useState(inicial) //le paso el valor inicial del parametro de useCOntador
-        const incrementar = () =>{
-            setContador(contador + 1)
-        }
-        return [contador, incrementar]
-
-    }
-
-    const App = () => {
-        const [contador, incrementar] = useContador(0)//se otorga el valor inicial
-        return(
-            <div>
-                Contador:{contador}
-                <button onClick={incrementar}>Incrementar</button>
-            </div>
-        )
-        }
-
-export default App;
-
-/* --- useState es una funcion que reotrn aun array, cuyo primer valor es un entero( en este caso ese valor entero
-es cero)
-El segundo valor del arreglo es una función(setContador), que nos permite actualiza el valor de contador(el valor inicial)
-
------> REGLAS DE LOS HOOKS <------
-siempre se llaman al principio del componente. Nunca se llaman en loops, ni en condiciones.
-Siempre se lllaman en el nivel más alto del componente.
-
-Siempre se llaman dentro de un componente de React
- o dentro de un custom hooks, -un hook customizado-
-
-un custom hook simpre comienza con la palaba "use"
+Si no colocamos el segundo argumento [], el useEffect se ejecutará simpre que haya algún cambio dentro del componente.
 
 */
+
+const useContador = (inicial) => {
+    const [contador, setContador] = useState(inicial) //le paso el valor inicial del parametro de useCOntador
+    const incrementar = () => {
+        setContador(contador + 1)
+    }
+    return [contador, incrementar]
+
+}
+
+const App = () => {
+    const [contador, incrementar] = useContador(0)//se otorga el valor inicial
+    useEffect(()=>{
+        document.title = contador;
+    }, [contador]) // entre corchetes, donde van las dependecias hago q el título de la página se actualce en base al contador.
+
+    return (
+        <div>
+            Contador:{contador}
+            <button onClick={incrementar}>Incrementar</button>
+        </div>
+    )
+}
+
+export default App;
---
+++ reactHooks/my-app/src/UseState.js
@@ -0,0 +1,72 @@
+import { useState, Component } from 'react'
+
+//USE STATE
+
+
+//-------> useState en componente Funcional <-------------
+
+// const App = () => {
+//     const [contador, setContador] = useState(0) //cero es el valor inicale
+//     return(
+//         <div>
+//             Contador:{contador}
+//             <button onClick={()=>setContador(contador+1)}>Incrementar</button>
+//         </div>
+//     )
+// }
+
+// useState en componente de clase
+// class App extends Component {
+//     state = {contador : 0}
+//     incrementar = () => {
+//         this.setState({contador:this.state.contador + 1})
+//     }
+//     render(){
+//         return (
+//             <div>
+//                 contador: {this.state.contador}
+//                 <button onClick={this.incrementar}>Incrementar</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+//----> CUSTOM HOOK <---------
+//Uso el mismo ejemplo de componente funcional, pero customizado.
+const useContador = (inicial) => {
+    const [contador, setContador] = useState(inicial) //le paso el valor inicial del parametro de useCOntador
+    const incrementar = () => {
+        setContador(contador + 1)
+    }
+    return [contador, incrementar]
+
+}
+
+const App = () => {
+    const [contador, incrementar] = useContador(0)//se otorga el valor inicial
+    return (
+        <div>
+            Contador:{contador}
+            <button onClick={incrementar}>Incrementar</button>
+        </div>
+    )
+}
+
+export default App;
+
+/* --- useState es una funcion que reotrn aun array, cuyo primer valor es un entero( en este caso ese valor entero
+es cero)
+El segundo valor del arreglo es una función(setContador), que nos permite actualiza el valor de contador(el valor inicial)
+
+-----> REGLAS DE LOS HOOKS <------
+siempre se llaman al principio del componente. Nunca se llaman en loops, ni en condiciones.
+Siempre se lllaman en el nivel más alto del componente.
+
+Siempre se llaman dentro de un componente de React
+ o dentro de un custom hooks, -un hook customizado-
+
+Un custom hook simpre comienza con la palaba "use".
+
+
+*/
